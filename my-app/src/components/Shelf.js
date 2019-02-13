d46481213b83b2eda43b9f9130e9a84ebf1337d5
Robert Chung <chungrob@gmail.com>
2019-02-12 19:47:32 -0500

    Update Shelf.js

--- my-app/src/components/Shelf.js
+++ my-app/src/components/Shelf.js
@@ -1,13 +1,18 @@
 import React, { Component } from 'react';
 
 function Shelf(props){
+    const allRows = props.shelf1.map((shelfRow)=> 
+        <div> {shelfRow.row} </div>
+    );
     return ( 
-        <div>
-            <div> THIS IS  A Test SHELF </div>
-            <div>{props.shelf1[0].row}</div>
-            <img src = {props.shelf1[0].shoes[0].imgLink} alt="TEST" />
-            <div>{props.shelf1[0].shoes[0].brand}</div>
-        </div>
+        <div> {allRows} </div>
+        /* TEST DIV */
+        // <div>
+        //     <div> THIS IS  A Test SHELF </div>
+        //     <div>{props.shelf1[0].row}</div>
+        //     <img src = {props.shelf1[0].shoes[0].imgLink} alt="TEST" />
+        //     <div>{props.shelf1[0].shoes[0].brand}</div>
+        // </div>
     )
 }
 
